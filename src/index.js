b0d55e15a2de04dcf6e9b9e90b7b5cd4ee9d8575
Andrew Johnson <manilda7@gmail.com>
2019-12-17 11:48:54 -0700

    Adds some functionality to see top word posted

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  $.get("https://wordwatch-api.herokuapp.com/api/v1/top_word", function(data){
+    document.querySelector('#demo').innerHTML = JSON.stringify(data.word)
+  });
 })
